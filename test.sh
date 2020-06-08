e89bce8b09a3469a4ef505eade18c6da1736e48f
szc126 <>
2020-06-08 14:37:28 -0700

    add test.sh

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#./sdl.py "ja:柳沢信大『粤東俗字便蒙解』.djvu" -o /tmp/sdl/ettou
+#./sdl.py "ko:重刊老乞大諺解 002.pdf" -o /tmp/sdl/nokeltay2
+#./sdl.py "mul:Émile Gérard - On diner à conte-cour, 1885 (in BSLLW s.2-t.7 p.263-265).djvu" # mul
+
+#./sdl.py "mul:Émile Gérard - On diner à conte-cour, 1885 (in BSLLW s.2-t.7 p.263-265).djvu/rif" # "/rif"? what?
+#./sdl.py "en:A Dialect of Donegal" # not a file
